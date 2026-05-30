0c5901b2bc7a642b65f63ee0628e58f44a7676f7
Jeremy Ellis <jeremyellis173@gmail.com>
2026-05-29 22:18:03 -0400

    fix: prop for _layout buttons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,18 @@
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { colors } from "../utils/styles";
-import { Tabs } from "expo-router";
+import { Tabs, usePathname } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import SolidButton from "../components/solidUI/SolidButton";
 
 export default function RootLayout() {
+  const pathname = usePathname();
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Tabs
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: colors.blue,
           tabBarStyle: {
             paddingTop: 5,
             height: 100,
@@ -22,18 +23,16 @@ export default function RootLayout() {
           name="People"
           options={{
             title: "People",
-            tabBarButton: ({ onPress, accessibilityState }) => (
+            tabBarButton: ({ onPress }) => (
               <SolidButton
                 isToggle
-                toggleValue={false}
+                toggleValue={pathname === "/People"}
                 onPress={() => onPress?.(undefined as any)}
                 child={
                   <MaterialCommunityIcons
                     name="cards"
                     size={24}
-                    color={
-                      accessibilityState?.selected ? colors.blue : colors.gray
-                    }
+                    color={pathname === "/People" ? colors.theme : colors.gray}
                   />
                 }
               />
@@ -44,18 +43,16 @@ export default function RootLayout() {
           name="Likes"
           options={{
             title: "Likes",
-            tabBarButton: ({ onPress, accessibilityState }) => (
+            tabBarButton: ({ onPress }) => (
               <SolidButton
                 isToggle
-                toggleValue={accessibilityState?.selected}
+                toggleValue={pathname === "/Likes"}
                 onPress={() => onPress?.(undefined as any)}
                 child={
                   <FontAwesome6
                     name="heart"
                     size={24}
-                    color={
-                      accessibilityState?.selected ? colors.blue : colors.gray
-                    }
+                    color={pathname === "/Likes" ? colors.theme : colors.gray}
                   />
                 }
               />
@@ -66,17 +63,17 @@ export default function RootLayout() {
           name="Messages"
           options={{
             title: "Messages",
-            tabBarButton: ({ onPress, accessibilityState }) => (
+            tabBarButton: ({ onPress }) => (
               <SolidButton
                 isToggle
-                toggleValue={accessibilityState?.selected}
+                toggleValue={pathname === "/Messages"}
                 onPress={() => onPress?.(undefined as any)}
                 child={
                   <MaterialCommunityIcons
                     name="message-outline"
                     size={24}
                     color={
-                      accessibilityState?.selected ? colors.blue : colors.gray
+                      pathname === "/Messages" ? colors.theme : colors.gray
                     }
                   />
                 }
@@ -88,18 +85,16 @@ export default function RootLayout() {
           name="Rank"
           options={{
             title: "Rank",
-            tabBarButton: ({ onPress, accessibilityState }) => (
+            tabBarButton: ({ onPress }) => (
               <SolidButton
                 isToggle
-                toggleValue={accessibilityState?.selected}
+                toggleValue={pathname === "/Rank"}
                 onPress={() => onPress?.(undefined as any)}
                 child={
                   <MaterialCommunityIcons
                     name="trophy-outline"
                     size={24}
-                    color={
-                      accessibilityState?.selected ? colors.blue : colors.gray
-                    }
+                    color={pathname === "/Rank" ? colors.theme : colors.gray}
                   />
                 }
               />
@@ -110,18 +105,16 @@ export default function RootLayout() {
           name="Profile"
           options={{
             title: "Profile",
-            tabBarButton: ({ onPress, accessibilityState }) => (
+            tabBarButton: ({ onPress }) => (
               <SolidButton
                 isToggle
-                toggleValue={accessibilityState?.selected}
+                toggleValue={pathname === "/Profile"}
                 onPress={() => onPress?.(undefined as any)}
                 child={
                   <FontAwesome6
                     name="circle-user"
                     size={24}
-                    color={
-                      accessibilityState?.selected ? colors.blue : colors.gray
-                    }
+                    color={pathname === "/Profile" ? colors.theme : colors.gray}
                   />
                 }
               />
